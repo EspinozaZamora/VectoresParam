e161d6bdebeea1fb0a2af060cf2ea06ce53d6ac4
Estudiante PUCESE <estudiante@pucese.edu.ec>
2018-12-06 09:20:03 -0500

    Vectores Como Parametros

--- VectoresParam/Program.cs
+++ VectoresParam/Program.cs
@@ -8,8 +8,33 @@ namespace VectoresParam
 {
     class Program
     {
+        const int MAX = 10;
         static void Main(string[] args)
         {
+
+            int[] x = new int[MAX];
+            leer(x);
+            imprimir(x);
+            Console.ReadKey();
         }
+
+        public static void leer( int [] x)
+        {
+            for( int i = 0; i < MAX; i++)
+            {
+                Console.WriteLine("\n Ingrese un Elemento {0}", i + 1);
+                x[i] = Int32.Parse(Console.ReadLine());
+
+            }
+        }
+        public static void imprimir(int[] x)
+        {
+            foreach ( int num in x)
+            {
+                Console.WriteLine("\n Elemento {0}", num);
+
+            }
+        }
+
     }
 }
